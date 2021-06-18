a43fa18d0e179b36e3b7564b9a2ea908b8f48c78
theabro <luke.abraham@gmail.com>
2021-06-18 13:33:49 +0100

    added initial info for next round of data to convert

---
+++ lotus/submit_ta_r2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash 
+#SBATCH --partition=short-serial
+#SBATCH -o %j.out 
+#SBATCH -e %j.err
+#SBATCH --time=24:00:00
+
+# run python
+module load jaspy/3.7
+python /home/users/nlabraham/git/ccmi2022-ukca/src/convert_pp2nc.py -f ta.json -r refD1_r2i1p1f2_apn.json -d ${DECADE}
