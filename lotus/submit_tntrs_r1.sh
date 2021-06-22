92fb14a3ae66b283f2b257ce4244e87f5f3c3274
theabro <luke.abraham@gmail.com>
2021-06-22 16:28:41 +0100

    further scripts

---
+++ lotus/submit_tntrs_r1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash 
+#SBATCH --partition=short-serial
+#SBATCH -o %j.out 
+#SBATCH -e %j.err
+#SBATCH --time=3:00:00
+
+# run python
+module load jaspy/3.7
+python /home/users/nlabraham/git/ccmi2022-ukca/src/convert_pp2nc.py -f tntrs.json -r refD1_r1i1p1f2_apn4.json -d ${DECADE}
