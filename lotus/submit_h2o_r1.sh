2555f7bb1dd98311a8a1dd5820f75c9812e56f83
theabro <luke.abraham@gmail.com>
2021-06-22 09:47:00 +0100

    tweaks to scripts

--- lotus/submit_h2o_r1.sh
+++ lotus/submit_h2o_r1.sh
@@ -1,10 +1,9 @@
 #!/bin/bash 
-#SBATCH --partition=high-mem
+#SBATCH --partition=short-serial
 #SBATCH -o %j.out 
 #SBATCH -e %j.err
-#SBATCH --time=48:00:00
-#SBATCH --mem=24000
+#SBATCH --time=3:00:00
 
 # run python
 module load jaspy/3.7
-python /home/users/nlabraham/git/ccmi2022-ukca/src/convert_pp2nc.py -f h2o.json -r refD1_r1i1p1f2_apn.json
+python /home/users/nlabraham/git/ccmi2022-ukca/src/convert_pp2nc.py -f h2o.json -r refD1_r1i1p1f2_apn.json -d ${DECADE}
