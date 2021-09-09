af26421d3588c15b73c89b15d1aa031803fb0224
theabro <luke.abraham@gmail.com>
2021-09-09 13:21:45 +0100

    added o3strat* files

---
+++ lotus/submit_all_o3strat.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# submit all the current fields to the lotus queues
+
+for i in `seq 1960 10 2010`; do echo $i; sbatch --export=ALL,DECADE=$i /home/users/nlabraham/git/ccmi2022-ukca/lotus/submit_o3strat_r1.sh ; done
+for i in `seq 1960 10 2010`; do echo $i; sbatch --export=ALL,DECADE=$i /home/users/nlabraham/git/ccmi2022-ukca/lotus/submit_o3strat_r2.sh ; done
+for i in `seq 1960 10 2010`; do echo $i; sbatch --export=ALL,DECADE=$i /home/users/nlabraham/git/ccmi2022-ukca/lotus/submit_o3strat_r3.sh ; done
+for i in `seq 1960 10 2010`; do echo $i; sbatch --export=ALL,DECADE=$i /home/users/nlabraham/git/ccmi2022-ukca/lotus/submit_o3stratloss_r1.sh ; done
+for i in `seq 1960 10 2010`; do echo $i; sbatch --export=ALL,DECADE=$i /home/users/nlabraham/git/ccmi2022-ukca/lotus/submit_o3stratloss_r2.sh ; done
+for i in `seq 1960 10 2010`; do echo $i; sbatch --export=ALL,DECADE=$i /home/users/nlabraham/git/ccmi2022-ukca/lotus/submit_o3stratloss_r3.sh ; done
+for i in `seq 1960 10 2010`; do echo $i; sbatch --export=ALL,DECADE=$i /home/users/nlabraham/git/ccmi2022-ukca/lotus/submit_o3stratdep_r1.sh ; done
+for i in `seq 1960 10 2010`; do echo $i; sbatch --export=ALL,DECADE=$i /home/users/nlabraham/git/ccmi2022-ukca/lotus/submit_o3stratdep_r2.sh ; done
+for i in `seq 1960 10 2010`; do echo $i; sbatch --export=ALL,DECADE=$i /home/users/nlabraham/git/ccmi2022-ukca/lotus/submit_o3stratdep_r3.sh ; done
