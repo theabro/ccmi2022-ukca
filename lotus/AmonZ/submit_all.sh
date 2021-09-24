eec16afccc809ba6a0b3c88c9383394ce6c2128d
theabro <luke.abraham@gmail.com>
2021-09-24 17:08:15 +0100

    indents

--- lotus/AmonZ/submit_all.sh
+++ lotus/AmonZ/submit_all.sh
@@ -30,7 +30,7 @@ d=n5
 for v in `echo "h2o"`; do
     for e in `seq 1 3`; do
 	for i in `seq 1960 10 2010`; do 
-	echo ${v} ${e} ${d} ${t} ${i}
-	sbatch --job-name=${v}_r${e}_${t}_${i} --export=ALL,VAR=${v},ENS=${e},DIR=${d},TAB=${t},DECADE=${i} --output=${t}_${v}_r${e}_${i}.out --error=${t}_${v}_r${e}_${i}.err /home/users/nlabraham/git/ccmi2022-ukca/lotus/AmonZ/submit_vedt_d.sh
+	    echo ${v} ${e} ${d} ${t} ${i}
+	    sbatch --job-name=${v}_r${e}_${t}_${i} --export=ALL,VAR=${v},ENS=${e},DIR=${d},TAB=${t},DECADE=${i} --output=${t}_${v}_r${e}_${i}.out --error=${t}_${v}_r${e}_${i}.err /home/users/nlabraham/git/ccmi2022-ukca/lotus/AmonZ/submit_vedt_d.sh
     done
 done
