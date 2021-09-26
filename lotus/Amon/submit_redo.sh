751ee0b57a6d4786f09c11bee18101a66a607556
theabro <luke.abraham@gmail.com>
2021-09-26 10:45:13 +0100

    switched to high-mem just in case

--- lotus/Amon/submit_redo.sh
+++ lotus/Amon/submit_redo.sh
@@ -2,10 +2,10 @@
 
 # resubmit all the failed fields to the lotus queues
 
-sbatch --export=ALL,DECADE=1990 /home/users/nlabraham/git/ccmi2022-ukca/lotus/Amon/submit_va_r1.sh
-sbatch --export=ALL,DECADE=1990 /home/users/nlabraham/git/ccmi2022-ukca/lotus/Amon/submit_wa_r2.sh
-sbatch --export=ALL,DECADE=1990 /home/users/nlabraham/git/ccmi2022-ukca/lotus/Amon/submit_va_r3.sh
-sbatch --export=ALL,DECADE=1960 /home/users/nlabraham/git/ccmi2022-ukca/lotus/Amon/submit_ta_r3.sh
-sbatch --export=ALL,DECADE=1960 /home/users/nlabraham/git/ccmi2022-ukca/lotus/Amon/submit_ua_r3.sh
-sbatch --export=ALL,DECADE=1970 /home/users/nlabraham/git/ccmi2022-ukca/lotus/Amon/submit_ua_r3.sh
-sbatch --export=ALL,DECADE=2010 /home/users/nlabraham/git/ccmi2022-ukca/lotus/Amon/submit_ua_r3.sh
+sbatch --export=ALL,DECADE=1990 /home/users/nlabraham/git/ccmi2022-ukca/lotus/Amon/submit_va_r1_hm.sh
+sbatch --export=ALL,DECADE=1990 /home/users/nlabraham/git/ccmi2022-ukca/lotus/Amon/submit_wa_r2_hm.sh
+sbatch --export=ALL,DECADE=1990 /home/users/nlabraham/git/ccmi2022-ukca/lotus/Amon/submit_va_r3_hm.sh
+sbatch --export=ALL,DECADE=1960 /home/users/nlabraham/git/ccmi2022-ukca/lotus/Amon/submit_ta_r3_hm.sh
+sbatch --export=ALL,DECADE=1960 /home/users/nlabraham/git/ccmi2022-ukca/lotus/Amon/submit_ua_r3_hm.sh
+sbatch --export=ALL,DECADE=1970 /home/users/nlabraham/git/ccmi2022-ukca/lotus/Amon/submit_ua_r3_hm.sh
+sbatch --export=ALL,DECADE=2010 /home/users/nlabraham/git/ccmi2022-ukca/lotus/Amon/submit_ua_r3_hm.sh
---
+++ lotus/Amon/submit_wa_r2_hm.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+#SBATCH --partition=high-mem
+#SBATCH -o %j.out 
+#SBATCH -e %j.err
+#SBATCH --time=24:00:00
+#SBATCH --mem=24000
+
+# run python
+module load jaspy/3.7
+python /home/users/nlabraham/git/ccmi2022-ukca/src/convert_pp2nc.py -f wa.json -r refD1_r2i1p1f2_apn4.json -d ${DECADE}
