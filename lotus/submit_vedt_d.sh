a9acb451294d07b0c650dad55c1eef507996069e
theabro <luke.abraham@gmail.com>
2021-09-27 16:59:41 +0100

    scripts for the ?a 3D fields

---
+++ lotus/submit_vedt_d.sh
@@ -0,0 +1,8 @@
+#!/bin/bash 
+#SBATCH --partition=high-mem
+#SBATCH --time=48:00:00
+#SBATCH --mem=24000
+
+# run python
+module load jaspy/3.7
+python /home/users/nlabraham/git/ccmi2022-ukca/src/convert_pp2nc.py -f ${VAR}.json -r refD1_r${ENS}i1p1f2_ap${DIR}.json -s ${TAB} -d ${DECADE}
